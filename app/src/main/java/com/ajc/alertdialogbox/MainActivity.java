1d39a4148c3a9b50b7eb6d9cbb6227383308cc8a
Avadhutjc <Avadhutjc@gmail.com>
2022-01-18 23:59:16 +0530

    AlertDialog box completed

---
+++ app/src/main/java/com/ajc/alertdialogbox/MainActivity.java
@@ -0,0 +1,50 @@
+package com.ajc.alertdialogbox;
+//https://www.youtube.com/watch?v=H3wAsORlnrY
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder a = new AlertDialog.Builder(MainActivity.this);
+        a.setMessage("Are you sure?")
+                .setPositiveButton("100%", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //coming out of main activity
+                        MainActivity.super.onBackPressed();
+                    }
+                })
+                .setNegativeButton("No", null);
+        AlertDialog c = a.create();
+        c.show();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
